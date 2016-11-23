a0330e1940d6d09a7e3bbe04292e69e8aeabab37
rbburkey <robinsonbburkey@gmail.com>
2016-11-23 13:53:22 -0500

    post controller and static views + some styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :posts
+
+  root 'static#home'
+
+  get '/about' => 'static#about'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
